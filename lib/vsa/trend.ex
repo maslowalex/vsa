14bbce0545df4b7b8b8e43b1c21aece03d7031f9
Alex Maslo <maslo.rails@gmail.com>
2024-10-18 19:37:46 +0300

    moved task in appropriate place

--- lib/vsa/trend.ex
+++ lib/vsa/trend.ex
@@ -14,37 +14,4 @@ defmodule VSA.Trend do
   def evaluate(_, close, sma, ema) when close <= sma and ema <= sma, do: :down
 
   def evaluate(_, _close, _sma, _ema), do: :flat
-
-  # def evaluate(%Context{bars: [%{trend: :down} | _]}, close, sma, ema)
-  #   when sma >= ema,
-  #   do: :flat
-
-  # def evaluate(%Context{bars: [%{trend: :down} | _]}, close, sma, ema)
-  #     when close >= sma and close >= ema,
-  #     do: :up
-
-  # def evaluate(%Context{bars: [%{trend: :down} | _]}, _close, _sma, _ema),
-  #   do: :down
-
-  # def evaluate(%Context{bars: [%{trend: :flat} | _]}, close, sma, ema)
-  #     when close >= sma and close >= ema and sma >= ema,
-  #     do: :up
-
-  # def evaluate(%Context{bars: [%{trend: :flat} | _]}, close, sma, ema)
-  #     when close <= sma and close >= ema and sma >= ema,
-  #     do: :down
-
-  # def evaluate(%Context{bars: [%{trend: :flat} | _]}, _, _, _),
-  #   do: :flat
-
-  # def evaluate(%Context{bars: [%{trend: :up} | _]}, close, sma, ema)
-  #     when close <= sma and close <= ema and sma <= ema,
-  #     do: :down
-
-  # def evaluate(%Context{bars: [%{trend: :up} | _]}, _close, sma, ema)
-  #     when sma <= ema,
-  #     do: :down
-
-  # def evaluate(%Context{bars: [%{trend: :up} | _]}, _close, _sma, _ema),
-  #   do: :flat
 end
